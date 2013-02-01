e03a2cdbe257e01fc5a7538b7672e03c78d15c22
Tomohiro Hosaka <bokutin@bokut.in>
2013-02-01 18:37:12 +0900

    ElapsedTimeをmojoliciousっぽく。(0.012907s, 77.477秒)

--- lib/BOKUTIN/TraitFor/Web/App/ElapsedTime.pm
+++ lib/BOKUTIN/TraitFor/Web/App/ElapsedTime.pm
@@ -8,14 +8,17 @@ around dispatch => sub {
     my $orig = shift;
     my $c = shift;
 
-    my $start   = [gettimeofday];
+    my $started = [gettimeofday];
     my $ret     = $c->$orig(@_);
-    my $end     = [gettimeofday];
-    my $elapsed = tv_interval($start, $end);
 
     if ($c->res->has_body and $c->res->content_type =~ m{^text/html}) {
-        my $sec = sprintf('%.2f', $elapsed);
-        $c->res->body( $c->res->body =~ s/ElapsedTime/$sec/r );
+        my $ended   = [gettimeofday];
+
+        my $elapsed = sprintf '%f', tv_interval($started, $ended);
+        my $rps     = $elapsed == 0 ? '??' : sprintf '%.3f', 1/$elapsed;
+        my $label   = "${elapsed}s, $rps";
+
+        $c->res->body( $c->res->body =~ s/ElapsedTime/$label/r );
     }
 
     $ret;
